215ade7aa58010ce88de07819b392f88f061b149
robertostoja-lniski <robert.lniski@gmail.com>
2020-05-17 18:01:47 +0200

    Aplication functions implemented
    It was done usign TDD, so tests are added
    with the same commit

--- include/Parser.h
+++ include/Parser.h
@@ -15,19 +15,19 @@ private:
     std::shared_ptr<SyntaxAnalyser> syntaxAnalyser;
     std::shared_ptr<Statement> currentParsedStatement;
     void generateCreateKeyOption();
-//    Token generatePrivateKeyOption();
-//    Token generateDeleteKeyOption();
-//    Token generateSignOption();
-//    Token generateCheckSignatureOption();
-//    Token generateEncryptFileOption();
+    void generatePrivateKeyOption();
+    void generateDeleteKeyOption();
+    void generateSignOption();
+    void generateCheckSignatureOption();
+    void generateEncryptFileOption();
 
     std::map<std::string, std::function<void()>> tokenGenerator {
             {"create-key", [&]() { return generateCreateKeyOption(); }},
-            {"create-key", [&]() { return generateCreateKeyOption(); }},
-            {"create-key", [&]() { return generateCreateKeyOption(); }},
-            {"create-key", [&]() { return generateCreateKeyOption(); }},
-            {"create-key", [&]() { return generateCreateKeyOption(); }},
-            {"create-key", [&]() { return generateCreateKeyOption(); }},
+            {"get-private-key", [&]() { return generatePrivateKeyOption(); }},
+            {"delete-key", [&]() { return generateDeleteKeyOption(); }},
+            {"sign", [&]() { return generateSignOption(); }},
+            {"check-signature", [&]() { return generateCheckSignatureOption(); }},
+            {"encrypt-file", [&]() { return generateEncryptFileOption(); }},
     };
 public:
     Parser(std::shared_ptr<SyntaxAnalyser> syntaxAnalyser) {
