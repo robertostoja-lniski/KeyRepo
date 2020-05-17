6e0769215e5c6e706eddd8f64b3a3a9d4d1769b8
robertostoja-lniski <robert.lniski@gmail.com>
2020-05-17 20:40:49 +0200

    Adding RSA key gen with OpenSSL support

--- source/Statement.cpp
+++ source/Statement.cpp
@@ -7,7 +7,7 @@
 std::ostream& operator<<(std::ostream& os, const CreateKeyStatement& dt)
 {
     os << "Create key with: " << dt.algorithm << " algorithm, " << dt.keyLen << " length of key "
-       << dt.pubKeyPath << " path to public key, " << dt.privateKeyId << " and private key id\n";
+       << dt.pubKeyPath << " path to public key, " << dt.privateKeyIdFile << " and private key id\n";
     return os;
 }
 
