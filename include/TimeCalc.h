3f073d729f0e1e541afb2165a25b373370385f42
robertostoja-lniski <robert.lniski@gmail.com>
2021-01-01 18:45:51 +0100

    TimeCalc

--- include/TimeCalc.h
+++ include/TimeCalc.h
@@ -21,22 +21,30 @@ public:
     virtual void readKeyTime(unsigned int trials) = 0;
     virtual void removeKeyTime(unsigned int trials) = 0;
     virtual void getKeySizeTime(unsigned int trials) = 0;
+    virtual void getKeyModeTime(unsigned int trials) = 0;
+    virtual void setKeyModeTime(unsigned int trials) = 0;
     virtual void getCurrentKeyNumFromEmulationTime(unsigned int trials) = 0;
-    virtual void unusedMapRowOptimisation(unsigned int trials) = 0;
+    virtual uint64_t unusedMapRowOptimisation(unsigned int trials) = 0;
+    virtual void defragmentationOptimisation(unsigned int trials, unsigned int scenario_id) = 0;
     virtual ~TimeCalc() = 0;
+    virtual void setTestedSize(uint64_t size) = 0;
 };
 
 
 class EmulationTimeCalc : public TimeCalc {
 private:
-    const uint64_t testedSize = 4096;
+    uint64_t testedSize {4096};
 public:
+    void setTestedSize(uint64_t size);
     void writeKeyTime(unsigned int trials);
     void readKeyTime(unsigned int trials);
     void removeKeyTime(unsigned int trials);
     void getKeySizeTime(unsigned int trials);
+    void getKeyModeTime(unsigned int trials);
+    void setKeyModeTime(unsigned int trials);
     void getCurrentKeyNumFromEmulationTime(unsigned int trials);
-    void unusedMapRowOptimisation(unsigned int trials);
+    uint64_t unusedMapRowOptimisation(unsigned int trials);
+    void defragmentationOptimisation(unsigned int trials, unsigned int scenario_id);
     ~EmulationTimeCalc() = default;
 };
 
