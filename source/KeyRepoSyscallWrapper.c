3c058fb5361ce4906436380194be78a425ef49af
robertostoja-lniski <robert.lniski@gmail.com>
2021-02-08 19:30:51 +0100

    Missing source file added

---
+++ source/KeyRepoSyscallWrapper.c
@@ -0,0 +1,168 @@
+//
+// Created by Robert Ostoja-Lniski on 1/7/21.
+//
+#include "../include/KeyRepoSyscallWrapper.h"
+
+// private structures and functions
+struct original_uids {
+    int uid;
+    int gid;
+};
+typedef struct original_uids original_uids;
+
+original_uids get_original_uids() {
+
+    const char* uid;
+    const char* gid;
+    int num_uid = -1;
+    int num_gid = -1;
+
+    uid = getenv("SUDO_UID");
+    if(uid == NULL) {
+        num_uid = (int)geteuid();
+    }
+
+    gid = getenv("SUDO_GID");
+    if(gid == NULL) {
+        num_gid = (int)getegid();
+    }
+
+    original_uids ret;
+    ret.uid = num_uid;
+    ret.gid = num_gid;
+    return ret;
+
+}
+
+#if EMULATION == 0
+
+int get_key_num() {
+    return syscall(__x64_get_key_num);
+}
+
+int write_key(const char* key, const uint64_t keyLen, uint64_t* id) {
+
+    uint64_t usedLen = keyLen;
+    if(keyLen > strlen(key)) {
+        usedLen = strlen(key);
+    }
+
+    original_uids ids = get_original_uids();
+    if(ids.uid == -1 || ids.gid == -1) {
+        return -1;
+    }
+
+    printf("Len is %zu\n", usedLen);
+    return syscall(__x64_write_key, key, usedLen, id, ids.uid, ids.gid);
+}
+
+int read_key(const uint64_t id, char* key, uint64_t keyLen) {
+
+    original_uids ids = get_original_uids();
+    if(ids.uid == -1 || ids.gid == -1) {
+        return -1;
+    }
+
+    return syscall(__x64_read_key, id, key, keyLen, ids.uid, ids.gid);
+}
+
+int remove_key(const uint64_t id) {
+
+    original_uids ids = get_original_uids();
+    if(ids.uid == -1 || ids.gid == -1) {
+        return -1;
+    }
+
+    return syscall(__x64_remove_key, id, ids.uid, ids.gid);
+}
+
+int get_key_size(const uint64_t id, uint64_t* size) {
+
+    original_uids ids = get_original_uids();
+    if(ids.uid == -1 || ids.gid == -1) {
+        return -1;
+    }
+
+    return syscall(__x64_get_key_size, id, size, ids.uid, ids.gid);
+}
+
+int get_mode(const uint64_t id, int* output) {
+
+    original_uids ids = get_original_uids();
+    if(ids.uid == -1 || ids.gid == -1) {
+        return -1;
+    }
+
+    return syscall(__x64_get_mode, id, output, ids.uid, ids.gid);
+}
+
+int set_mode(const uint64_t id, int new_mode) {
+
+    original_uids ids = get_original_uids();
+    if(ids.uid == -1 || ids.gid == -1) {
+        return -1;
+    }
+
+    return syscall(__x64_set_mode, id, new_mode, ids.uid, ids.gid);
+}
+#else
+
+#include "../include/KernelEmulation.h"
+
+int write_key(const char* key, const uint64_t keyLen, uint64_t* id) {
+
+    original_uids ids = get_original_uids();
+    if(ids.uid == -1 || ids.gid == -1) {
+        return -1;
+    }
+
+    return do_write_key(key, keyLen, id, ids.uid, ids.gid);
+}
+int read_key(const uint64_t id, char* key, uint64_t keyLen) {
+    original_uids ids = get_original_uids();
+    if(ids.uid == -1 || ids.gid == -1) {
+        return -1;
+    }
+
+    return do_read_key(id, key, keyLen, ids.uid, ids.gid);
+}
+int remove_key(const uint64_t id) {
+    original_uids ids = get_original_uids();
+    if(ids.uid == -1 || ids.gid == -1) {
+        return -1;
+    }
+
+    return do_remove_key(id, ids.uid, ids.gid);
+}
+int get_key_size(const uint64_t id, uint64_t* size) {
+
+    original_uids ids = get_original_uids();
+    if(ids.uid == -1 || ids.gid == -1) {
+        return -1;
+    }
+
+    return do_get_key_size(id, size, ids.uid, ids.gid);
+}
+int get_mode(const uint64_t id, int* output) {
+
+    original_uids ids = get_original_uids();
+    if(ids.uid == -1 || ids.gid == -1) {
+        return -1;
+    }
+
+    return do_get_mode(id, output, ids.uid, ids.gid);
+}
+int set_mode(const uint64_t id, int new_mode) {
+
+    original_uids ids = get_original_uids();
+    if(ids.uid == -1 || ids.gid == -1) {
+        return -1;
+    }
+
+    return do_set_mode(id, new_mode, ids.uid, ids.gid);
+}
+int get_key_num() {
+    return do_get_key_num();
+}
+
+#endif
