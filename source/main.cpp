c34f04efd45029feb81b63703d258dca33b97f21
robertostoja-lniski <robert.lniski@gmail.com>
2022-07-05 09:58:28 +0200

    building lib setup

--- source/main.cpp
+++ source/main.cpp
@@ -5,16 +5,8 @@
 #include <memory>
 
 int main(int argc, char** argv) {
-
-    std::shared_ptr<Executor> executor;
-    try {
-        auto lexer = std::make_shared<Lexer>(argc, argv);
-        auto parser = std::make_shared<Parser>(lexer);
-        executor = std::make_shared<Executor>(parser);
-        executor->execute();
-    } catch(std::exception &e) {
-        std::cout << e.what();
-        executor->printHelp();
-    }
+    EmulationTimeCalc timeCalc;
+    timeCalc.setTestedSize(2048);
+    timeCalc.removeKeyTime(200);
 
 }
