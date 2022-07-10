8e93a289b6ad6243a8254ad01f919d160ca15c8c
robertostoja-lniski <robert.lniski@gmail.com>
2022-07-10 17:37:18 +0200

    Help function updated

--- source/main.cpp
+++ source/main.cpp
@@ -6,21 +6,21 @@
 
 int main(int argc, char** argv) {
 
-//    std::shared_ptr<Executor> executor;
-//    try {
-//        auto lexer = std::make_shared<Lexer>(argc, argv);
-//        auto parser = std::make_shared<Parser>(lexer);
-//        executor = std::make_shared<Executor>(parser);
-//        executor->execute();
-//    } catch(std::exception &e) {
-//        std::cout << e.what();
-//        executor->printHelp();
-//    }
+    std::shared_ptr<Executor> executor;
+    try {
+        auto lexer = std::make_shared<Lexer>(argc, argv);
+        auto parser = std::make_shared<Parser>(lexer);
+        executor = std::make_shared<Executor>(parser);
+        executor->execute();
+    } catch(std::exception &e) {
+        std::cout << e.what();
+        executor->printHelp();
+    }
 
-    EmulationTimeCalc timeCalc;
-    timeCalc.setTestedSize(2048);
-    timeCalc.test(200);
-    timeCalc.setTestedSize(4096);
-    timeCalc.test(200);
+//    EmulationTimeCalc timeCalc;
+//    timeCalc.setTestedSize(2048);
+//    timeCalc.test(200);
+//    timeCalc.setTestedSize(4096);
+//    timeCalc.test(200);
 
 }
