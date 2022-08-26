66374acc1c252ad01bbea585b236e28de7e1e3e6
robertostoja-lniski <robert.lniski@gmail.com>
2022-08-26 23:30:30 +0200

    Custom setup update

--- setup/custom_setup.sh
+++ setup/custom_setup.sh
@@ -1,10 +1,22 @@
-rm -rf linux*
-tar -xvf ../linux-5.4.80.tar.xz -C .
-src=src/KeyRepo/setup/changed_files_in_kernel
-mkdir linux-5.4.80/krepo
-cp $src/krepo/* linux-5.4.80/krepo
-cp src/KeyRepo/source/KernelEmulation.c linux-5.4.80/krepo/krepo.c
-cp src/KeyRepo/include/KeyRepoHeaders.h linux-5.4.80/krepo
-cp $src/Makefile linux-5.4.80/
-cp $src/include/linux/syscalls.h linux-5.4.80/include/linux/syscalls.h
-cp $src/arch/x86/entry/syscalls/syscall_64.tbl linux-5.4.80/arch/x86/entry/syscalls/syscall_64.tbl
+if [ ! -d new_kernel ]; then
+  mkdir new_kernel
+fi
+
+
+LINUX_KERNEL_NAME=linux-5.4.80
+FILE=new_kernel/$LINUX_KERNEL_NAME.tar.xz
+if [ ! -f "$FILE" ]; then
+    wget https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/$LINUX_KERNEL_NAME.tar.xz -O new_kernel/$LINUX_KERNEL_NAME.tar.xz
+else
+  rm -rf new_kernel/$LINUX_KERNEL_NAME
+fi
+
+tar -xvf new_kernel/linux-5.4.80.tar.xz -C new_kernel/
+mkdir new_kernel/linux-5.4.80/krepo
+cp setup/changed_files_in_kernel/krepo/* new_kernel/linux-5.4.80/krepo
+cp source/KernelEmulation.c new_kernel/linux-5.4.80/krepo/krepo.c
+sed -i 's/define EMULATION 1/define EMULATION 0/' new_kernel/linux-5.4.80/krepo/krepo.c
+cp include/KeyRepoHeaders.h new_kernel/linux-5.4.80/krepo
+cp setup/changed_files_in_kernel/Makefile new_kernel/linux-5.4.80/
+cp setup/changed_files_in_kernel/include/linux/syscalls.h new_kernel/linux-5.4.80/include/linux/syscalls.h
+cp setup/changed_files_in_kernel/arch/x86/entry/syscalls/syscall_64.tbl new_kernel/linux-5.4.80/arch/x86/entry/syscalls/syscall_64.tbl
